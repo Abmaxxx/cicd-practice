1116f227395f44b263a42593119840bcf43b56d9
Abdullah <bsee22057@itu.edu.pk>
2026-08-03 14:20:56 +0500

    the change

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,7 +5,7 @@ describe('GET /', () => {
     it('should return running message', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
-        expect(res.body.message).toBe('Server is running');
+        expect(res.body.message).toBe('CI/CD Pipeline Working 🚀');
     });
 });
 
